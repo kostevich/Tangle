39330edb4c4488bdd53e2741d6f166cea6b25c17
Irisha <irisha.kostevich@mail.ru>
2023-10-26 20:53:15 +0300

    Create MenuBar
    Panel of instruments on top.

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLayoutItem
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QToolBar, QStatusBar
+
+from PyQt6.QtGui import QAction
 
 # Создание приложения.
 Tangle = QApplication([])
@@ -12,6 +14,22 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         # Имя приложения.
         self.setWindowTitle("Tangle")
+        
+        Lable = QLabel("Repository", self)
+        Lable.move(250, 100)
+
+        # Создание панели инструментов.
+        PanelMenu = QToolBar("Показать/Скрыть панель инструментов")
+        # Создание кнопки на панели инструментов.
+        Templates = QAction("Templates", self)
+        ApplicationSettings = QAction("ApplicationSettings", self)
+        # Добавление кнопки на панель.
+        PanelMenu.addAction(Templates)
+        PanelMenu.addAction(ApplicationSettings)
+        # Отображение панели меню.
+        self.addToolBar(PanelMenu)
+        
+
 
 # Создание окошка.
 window = MainWindow()
