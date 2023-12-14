36f403d09e24928520189b89594bc0b2a33d3d88
Irisha <irisha.kostevich@mail.ru>
2023-12-14 20:48:28 +0300

    Refactoring

--- Main.py
+++ Main.py
@@ -1,13 +1,38 @@
 #!/usr/bin/python
 
-from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QWidget, QListWidget, QLabel, QToolBar, QPushButton
-from PyQt6.QtGui import QAction
-from PyQt6.QtCore import Qt 
+#==========================================================================================#
+# >>>>> ПОДКЛЮЧЕНИЕ БИБЛИОТЕК И МОДУЛЕЙ <<<<< #
+#==========================================================================================#
+
+from dublib.Methods import CheckPythonMinimalVersion
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QAction 
+from PyQt6.QtWidgets import QApplication, QHBoxLayout, QLabel, QListWidget, QMainWindow, QPushButton, QToolBar, QVBoxLayout, QWidget
+
+#==========================================================================================#
+# >>>>> ИНИЦИАЛИЗАЦИЯ СКРИПТА <<<<< #
+#==========================================================================================#
+
+# Проверка поддержки используемой версии Python.
+CheckPythonMinimalVersion(3, 11)
+
+#==========================================================================================#
+# >>>>> ЧТЕНИЕ НАСТРОЕК <<<<< #
+#==========================================================================================#
 
 ListRepository = ("1", "2", "3", "4", "5",)
 FunctionRepository = ("уДАИЛАСМВ", "2", )
+
+#==========================================================================================#
+# >>>>> ИНИЦИАЛИЗАЦИЯ ПРИЛОЖЕНИЯ <<<<< #
+#==========================================================================================#
+
 # Создание приложения.
 Tangle = QApplication([])
+
+#==========================================================================================#
+# >>>>> СОЗДАНИЕ ПРИЛОЖЕНИЯ <<<<< #
+#==========================================================================================#
 
 # Создание интерфейса приложения на основе класса QMainWindow.
 class MainWindow(QMainWindow):
@@ -17,6 +42,7 @@ class MainWindow(QMainWindow):
         # Имя приложения.
         self.setWindowTitle("Tangle")
         
+        # Создание подложки.
         self.LayoutLeft = QVBoxLayout()
         self.LayoutUnder = QHBoxLayout()
 
@@ -25,14 +51,17 @@ class MainWindow(QMainWindow):
 
         # Создание списка репозиториев.
         self.List = QListWidget(self)
+
         # Фиксированный размер окошка списка.
         self.List.setFixedSize(300, 500)
+
         # Добавление элементов списка.
         self.List.addItems(ListRepository)
 
         # Создание кнопочек для работы репозиториев.
         self.ButtonAdd = QPushButton("Добавить")
         self.ButtonDelete = QPushButton("Удалить")
+
         # Фиксированный размер кнопок.
         self.ButtonAdd.setFixedSize(120, 25)
         self.ButtonDelete.setFixedSize(120, 25)
@@ -40,6 +69,7 @@ class MainWindow(QMainWindow):
         # Добавление кнопок на макет.
         self.LayoutUnder.addWidget(self.ButtonAdd)
         self.LayoutUnder.addWidget(self.ButtonDelete)
+
         # Растягиваем пространство за кнопочками.
         self.LayoutUnder.addStretch(0)
 
@@ -50,28 +80,43 @@ class MainWindow(QMainWindow):
     
         # Создание объекта класса виджет.
         widget = QWidget()
+
         # Добавление макета на виджет.
         widget.setLayout(self.LayoutLeft)
+
         # Преобразовать среднее положение для макета.
         self.setCentralWidget(widget)
+
         # Добавление дополнительного макета на основной.
         self.LayoutLeft.addLayout(self.LayoutUnder)
 
         # Создание панели инструментов.
         PanelMenu = QToolBar("Показать/Скрыть панель инструментов")
+
         # Создание кнопки на панели инструментов.
         Templates = QAction("Templates", self)
+
         ApplicationSettings = QAction("ApplicationSettings", self)
+
         # Добавление кнопки на панель.
         PanelMenu.addAction(Templates)
         PanelMenu.addAction(ApplicationSettings)
+
         # Отображение панели меню.
         self.addToolBar(PanelMenu)
+
+#==========================================================================================#
+# >>>>> СОЗДАНИЕ ОКОШКА ПРИЛОЖЕНИЯ <<<<< #
+#==========================================================================================#
 
 # Создание окошка.
 window = MainWindow()
 # Отображение окошка.
 window.show()
+
+#==========================================================================================#
+# >>>>> ОБРАБОТКА СОБЫТИЙ ПРИЛОЖЕНИЯ <<<<< #
+#==========================================================================================#
 
 # Обработка событий происходящих в окошке.
 Tangle.exec()
---
+++ README.md
@@ -0,0 +1,27 @@
+# Tangle
+**Tangle** – приложение на [PyQt6](https://github.com/piotrkowalczuk/pqt?ysclid=lq2khsyn8x955167959), которое помогает работать с репозиториями на GitHub.
+
+# Порядок установки и использования
+1. Загрузить репозиторий. Распаковать. 
+2. Установить [Python](https://www.python.org/downloads/) версии не старше 3.11. Рекомендуется добавить в PATH.
+3. В среду исполнения установить следующие пакеты: [dublib](https://github.com/DUB1401/dublib), [PyQt6](https://github.com/piotrkowalczuk/pqt?ysclid=lq2khsyn8x955167959). 
+```
+pip install git+https://github.com/DUB1401/dublib
+pip install pyqt6
+```
+Либо установить сразу все пакеты при помощи следующей команды, выполненной из директории скрипта.
+```
+pip install -r requirements.txt
+``` 
+4. Запустить файл _main.py_.
+```
+python main.py
+```
+
+# Пример работы
+
+**Общий вид приложения:**
+
+![image](https://github.com/kostevich/RegistrationFromApp/assets/109979502/529dff2c-afda-437e-b739-2518b1803c13)
+
+_Copyright © Kostevich Irina. 2023._
